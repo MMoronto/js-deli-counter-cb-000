b64e9c93d9ce3cff13096b6acb44a613eae1ea97
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-29 11:03:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ var katzDeliLine = [];
 
 
 
-function currentLine(line) {
+function currentLine(array) {
   if (!line.length) {
     return "The line is currently empty."
   }
   const numbersAndNames = []
   
-  for (let i = 0; i < array.length; i++ )
+  for (let i = 0; i < array.length; i++ ) {
+    numbersAndNames.push(`${ i + 1 }. ${array[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
 
 
-function nowServing(line) {
+function nowServing(array) {
   
 }
 
-function takeANumber(line, name) {
+function takeANumber(array, name) {
   for (let i = 0; i < array.length; i++) {
     array.push(`Welcome, ${name[i]}. You are number${ i + 1 } in line.`)
   }
