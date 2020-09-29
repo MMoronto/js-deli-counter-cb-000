825e229ff4eedaa854fcf0be7ba43395b3ed6432
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-29 10:18:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var katzDeliLine = [];
 
-function takeANumber(people, name) {
+function takeANumber(katzDeliLine, name) {
   for (let = i = 0; i < array.length; i++) {
     array.push(`Welcome, ${name[i]}. You are number${ i + 1 } in line.`)
   }
+}
+
+function nowServing(katzDeliLine) {
+  
+}
+
+function currentLine(katzDeliLine) {
+  
 }
