7c942e4118ef5f6a38761ae04034a17a054253fd
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-29 10:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  for (let = i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     array.push(`Welcome, ${name[i]}. You are number${ i + 1 } in line.`)
   }
 }
